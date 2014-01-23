7bc7feba24ef6605d8a69757d4a1ed55cca1b630
Rafael Bravo <rafael@metheora.com>
2014-01-23 04:06:54 -0200

    ios detect

--- sample-geolocation/scripts/main.js
+++ sample-geolocation/scripts/main.js
@@ -8,7 +8,8 @@ function onDeviceReady() {
 	navigator.splashscreen.hide();
     init();
     try{
-		isiOS = device.platform=="iOS";
+    	var deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+		isiOS = deviceType=="iPhone"||deviceType=="iPad";
     } catch(e){ console.log(e);}
 }
 function init(){
