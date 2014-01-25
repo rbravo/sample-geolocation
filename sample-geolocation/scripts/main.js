f514b7f62017f68fcb271a5a269fe9e4482db3bf
Rafael Bravo <rafael@metheora.com>
2014-01-25 21:36:33 -0200

    alert again

--- sample-geolocation/scripts/main.js
+++ sample-geolocation/scripts/main.js
@@ -7,13 +7,14 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	navigator.splashscreen.hide();
     init();
-    try{
+}
+function init(){
+	try{
     	var deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
 		isiOS = deviceType=="iPhone"||deviceType=="iPad";
 		alert('deviceType: '+deviceType);
     } catch(e){ console.log(e);}
-}
-function init(){
+	
 	geolocationApp = new geolocationApp();
 	geolocationApp.run();
 	var img = new Image();
